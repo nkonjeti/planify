fdea6edc6a7e98b36dd042a6f257aa7ac9a92be7
nehakonjeti <nkonjeti@gmail.com>
2020-08-12 17:30:58 -0500

    neha - I put my questions in for classes & started timer display

--- src/Chatbot/config.js
+++ src/Chatbot/config.js
@@ -3,7 +3,7 @@ import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
   botName: "Bliss",
-  initialMessages: [createChatBotMessage(`Hello World`)],
+  initialMessages: [createChatBotMessage("Hi, I'm Bliss! I'm here to be your mentor!")],
 };
 
 export default config;
--- src/Timer.js
+++ src/Timer.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import './App.css';
+//import './App.css';
+import './Timer.css';
 
 function Timer() {
   return (
-    <div>
-      <h1>Timer </h1>
-    </div>
+  <section>
+    <section className = "timer">
+      <div>
+        
+        <h2>Break Time!</h2>
+        <p>Countdown for your break. You deserve it.</p>
+      </div>
+      <div>
+        <section>
+          <p>87</p>
+          <p><small>Minutes</small></p>
+
+        </section>
+        <span>:</span>
+        <section>
+          <p>87</p>
+          <p><small>Seconds</small></p>
+
+        </section>
+      </div>
+
+    </section>
+    </section>
   );
-}
+};
 
 export default Timer;
