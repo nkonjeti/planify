91a504b4da6eb8564147b3fd7f3877a57f6e7cdc
rena987 <47072485+rena987@users.noreply.github.com>
2020-08-12 20:27:49 -0400

    commit

--- src/Chatbot/ActionProvider.js
+++ src/Chatbot/ActionProvider.js
@@ -34,11 +34,65 @@ class ActionProvider {
         this.addMessageToState(message);
 
     };
-    
 
-   
-
+    socialIntro = () => {
+      const message = this.createChatBotMessage("So, what about your social life would you like to reflect on? Choose from these options that fits you most: 1. Peer Pressure, 2. Exclusion (Type a number)");
+      this.addMessageToState(message);
+    }
+    peerPressure = () => {
+      const message = this.createChatBotMessage("What does your situation involve? 5. Drugs? (Type a number)");
+      this.addMessageToState(message);
+    }
     
+    doIt = () => {
+      const message = this.createChatBotMessage("If a simple no isn't doing the trick, come up with a few excuses. Are you on a team of any kind?");
+      this.addMessageToState(message);
+    }
+
+    maximize = () => {
+      const message = this.createChatBotMessage("Then maybe say you're trying to stay healthy, whether its mentally or physically! Nobody would argue that a hangover would help you perform your best.");
+      this.addMessageToState(message);
+    }
+
+    maximization = () => {
+      const message = this.createChatBotMessage("Thats okay! You could simply offer to be the designated driver or keep a bottled drink like a soda or iced tea to drink at parties to avoid being offered. All else fails, blame your parents!");
+      this.addMessageToState(message);
+    }
+
+    exclusion = () => {
+      const message = this.createChatBotMessage("What does this situation of exclusion involve? 6. Friends, 7. Workplace (Type a number)");
+      this.addMessageToState(message);
+    }
+
+    friends = () => {
+      const message = this.createChatBotMessage("Aww I'm sorry you feel this way ;( Are you sure or unsure about your friends intentions? ");
+      this.addMessageToState(message);
+    }
+
+    sure = () => {
+      const message = this.createChatBotMessage("Just know that it's not your fault. Don't go changing yourself to fit in! Instead, work on building your self confidence and maybe re-evaluating your friendships to see if it was actually worth it.");
+      this.addMessageToState(message);
+    }
+
+    unsure = () => {
+      const message = this.createChatBotMessage("It's possible that you're overthinking it! Maybe reach out to your friends about these feelings since it's better than letting these assumptions eat you alive.");
+      this.addMessageToState(message);
+    }
+
+    work = () => {
+      const message = this.createChatBotMessage("Aww I'm sorry you feel this way ;( Does this involve work or colleagues?");
+      this.addMessageToState(message);
+    }
+
+    work2 = () => {
+      const message = this.createChatBotMessage("Maybe try sharpening your skills so you become indispensable! Or you could focus on finding office allies rather than friends who will complement your skills in the workplace. If all fails, maybe your current workplace culture doesn't suit you and it maybe time to explore other options. ");
+      this.addMessageToState(message);
+    }
+
+    work3 = () => {
+      const message = this.createChatBotMessage("If you feel out of the loop, maybe plan your own outing and invite your colleagues. Taking the lead will automatically become a crucial part of the gathering. Also, maybe tap in to the preferred communication methods of your workplace!");
+      this.addMessageToState(message);
+    }
   
 
     addMessageToState = (message) => {
--- src/Chatbot/MessageParser.js
+++ src/Chatbot/MessageParser.js
@@ -22,6 +22,39 @@ class MessageParser {
      else if(lowercase.includes("are you a robot")){
         this.actionProvider.robot();
       }
+      else if(lowercase.includes("Peer") || lowercase.includes("1")) {
+        this.actionProvider.peerPressure();
+      }
+      else if(lowercase.includes("Exclusion") || lowercase.includes("2")) {
+        this.actionProvider.exclusion();
+      }
+      else if (lowercase.includes("5")){
+        this.actionProvider.doIt();
+      }
+      else if (lowercase.includes("yes")) {
+        this.actionProvider.maximize();
+      }
+      else if (lowercase.includes("no")) {
+        this.actionProvider.maximization();
+      }
+      else if (lowercase.includes("6")) {
+        this.actionProvider.friends();
+      }
+      else if (lowercase.includes("sure")) {
+        this.actionProvider.sure();
+      }
+      else if (lowercase.includes("unsure")) {
+        this.actionProvider.unsure();
+      }
+      else if (lowercase.includes("7")) {
+        this.actionProvider.work();
+      }
+      else if (lowercase.includes("work")) {
+        this.actionProvider.work2();
+      }
+      else if (lowercase.includes("colleagues")) {
+        this.actionProvider.work3();
+      }
 
      //emotions
 
@@ -32,8 +65,8 @@ class MessageParser {
 
       //this would direct to planned reflection questions
       else{
-        if(response == 1){
-          this.actionProvider.greet();
+        if(response === 1){
+          this.actionProvider.socialIntro();
           response++;
          
         }
@@ -57,7 +90,12 @@ class MessageParser {
 
 
         //Serena Social Life here
+        /*else if (response === 5) {
+          this.actionProvider.socialIntro();
+          response++;
 
+
+        }*/
 
 
 
