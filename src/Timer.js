531f95bb54e8c036b1332379222eff9fa7681d7d
nehakonjeti <nkonjeti@gmail.com>
2020-08-13 14:32:12 -0500

    deleted timer code!

--- src/Timer.js
+++ src/Timer.js
@@ -2,69 +2,22 @@
 import React from 'react';
 //import './App.css';
 import './Timer.css';
-var time = 17 * 60;
-var minutes = 17;
-var seconds = 0;
+
 
  function Timer() {
   
   
-const updateCountdown = () => {
-    minutes = Math.floor(time/60);
-    seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds: seconds;
-    if( document.getElementById('countdown') != null  && document.getElementById('updateseconds') != null) {
-    document.getElementById('countdown').innerHTML = `${minutes}`;
-    document.getElementById('updateseconds').innerHTML = `${seconds}`;
-    
-    }
-   else if( document.getElementById('countdown') == null  && document.getElementById('updateseconds') == null) { 
-    time = 17 * 60;
-     minutes = 17;
-    seconds = 0;
 
-   }
-  
-   time--;
-   
-   };
-   
-   setInterval(updateCountdown, 1000);
-   
+
    
    return (
-    
   
- <section>
-   <section className = "timer">
       <div>
        
-      <h2>Break Time!</h2>
-       <p>Countdown for your break. You deserve it.</p>
+      <h1> timer</h1>
       </div>
-     <div>
-       <section>
-         <p id ="countdown">17</p>
-         <p><small>Minutes</small></p>
-
-       </section>
-       
-       <span>:</span>
-      
-      
-       <section>
-         <p id = "updateseconds">00</p>
-         <p><small>Seconds</small></p>
-
-       </section>
-     </div>
-
-   </section>
-   </section>
-   );
-
-   
-  
+    
+   )
 };
  
  export default Timer;
