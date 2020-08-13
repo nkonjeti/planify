6f24f825b87afcbb7ff373945eafe180530c5fa6
nehakonjeti <nkonjeti@gmail.com>
2020-08-13 18:50:37 -0500

    added spotify playlists:)

--- src/Music.js
+++ src/Music.js
@@ -5,10 +5,24 @@ import './Music.css';
 
  function Music() {
      return (
+         <div>
   <div>
-      <h1> Music </h1>
+      <h1> confidence boost</h1>
+      <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4fpCWaHOned?si=lFLqi3eZSEuW3axI3e5Ohw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
-
+  <div>
+  <h1> feeling fuzzy </h1>
+  <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?si=yYUvN-SQSNSIhk9Ahbe3Gw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+<div>
+  <h1> Nostalgic</h1>
+  <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd?si=HT9iy1utSmelbxnBgopIyQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+<div>
+  <h1> Bollywood </h1>
+  <iframe src="https://open.spotify.com/embed/playlist/2yKxZiKvysgl5NBSPD4XKQ?si=PSxvySUeRrSmkq3ZignzVw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+</div>
      );
  }
  
