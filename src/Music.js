1f03dc5d13d5828a67e0736b093afcbcb8c82594
Gautami Kankipati <kankipatigautami@gmail.com>
2020-08-14 11:26:52 -0500

    border

--- src/Music.js
+++ src/Music.js
@@ -8,23 +8,23 @@ import './Music.css';
     <body>
          <div>
   <div>
-      <h1> confidence boost</h1>
+      <h1> CONFIDENCE BOOST</h1>
       <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4fpCWaHOned?si=lFLqi3eZSEuW3axI3e5Ohw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
   <div>
-  <h2> feeling fuzzy </h2>
+  <h2> FEELING FUZZY </h2>
   <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?si=yYUvN-SQSNSIhk9Ahbe3Gw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h3> Nostalgic</h3>
+  <h3> NOSTALGIC</h3>
   <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd?si=HT9iy1utSmelbxnBgopIyQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h4> Bollywood </h4>
+  <h4> BOLLYWOOD</h4>
   <iframe src="https://open.spotify.com/embed/playlist/2yKxZiKvysgl5NBSPD4XKQ?si=PSxvySUeRrSmkq3ZignzVw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h5> Lo-fi </h5>
+  <h5> LO-FI </h5>
   <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?si=JeYZx3p2RlS0q4i7uWyDNA" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 </div>
