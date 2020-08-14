de3431b45f574ee7082108f9cf92581158d643e0
Gautami Kankipati <kankipatigautami@gmail.com>
2020-08-14 11:35:40 -0500

    border

--- src/Music.js
+++ src/Music.js
@@ -9,23 +9,23 @@ import './Music.css';
          <div>
   <div>
       <h1> CONFIDENCE BOOST</h1>
-      <iframe classname="frame1" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4fpCWaHOned?si=lFLqi3eZSEuW3axI3e5Ohw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe className="frame1" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4fpCWaHOned?si=lFLqi3eZSEuW3axI3e5Ohw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
   <div>
   <h2> FEELING FUZZY </h2>
-  <iframe classname="frame2" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?si=yYUvN-SQSNSIhk9Ahbe3Gw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <iframe className="frame2" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?si=yYUvN-SQSNSIhk9Ahbe3Gw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
   <h3> NOSTALGIC</h3>
-  <iframe classname="frame3" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd?si=HT9iy1utSmelbxnBgopIyQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <iframe className="frame3" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd?si=HT9iy1utSmelbxnBgopIyQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
   <h4> BOLLYWOOD</h4>
-  <iframe classname="frame4" src="https://open.spotify.com/embed/playlist/2yKxZiKvysgl5NBSPD4XKQ?si=PSxvySUeRrSmkq3ZignzVw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <iframe className="frame4" src="https://open.spotify.com/embed/playlist/2yKxZiKvysgl5NBSPD4XKQ?si=PSxvySUeRrSmkq3ZignzVw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
   <h5> LO-FI </h5>
-  <iframe classname="frame5" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?si=JeYZx3p2RlS0q4i7uWyDNA" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <iframe className="frame5" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?si=JeYZx3p2RlS0q4i7uWyDNA" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 </div>
 </body>
