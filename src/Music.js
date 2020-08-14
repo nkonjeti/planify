bbdc8a36c3a6d505fcaeea6488eb1bd601334393
nehakonjeti <nkonjeti@gmail.com>
2020-08-14 13:34:50 -0500

    spacing in spotify

--- src/Music.js
+++ src/Music.js
@@ -8,23 +8,23 @@ import './Music.css';
     <body>
          <div>
   <div>
-      <h1> CONFIDENCE BOOST</h1>
+      <h1 className = "boost"> CONFIDENCE BOOST</h1>
       <iframe className="frame1" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4fpCWaHOned?si=lFLqi3eZSEuW3axI3e5Ohw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
   <div>
-  <h2> FEELING FUZZY </h2>
+  <h2 className = "fuzzy"> FEELING FUZZY </h2>
   <iframe className="frame2" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?si=yYUvN-SQSNSIhk9Ahbe3Gw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h3> NOSTALGIC</h3>
+  <h3 className = "nostalgic"> NOSTALGIC</h3>
   <iframe className="frame3" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd?si=HT9iy1utSmelbxnBgopIyQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h4> BOLLYWOOD</h4>
+  <h4 className = "bolly"> BOLLYWOOD</h4>
   <iframe className="frame4" src="https://open.spotify.com/embed/playlist/2yKxZiKvysgl5NBSPD4XKQ?si=PSxvySUeRrSmkq3ZignzVw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 <div>
-  <h5> LO-FI </h5>
+  <h5 className = "lo-fi"> LO-FI </h5>
   <iframe className="frame5" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?si=JeYZx3p2RlS0q4i7uWyDNA" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 </div>
